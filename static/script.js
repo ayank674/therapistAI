6e99cbc1f28d23ff1743685609bf8cc94a5ac02a
FozzyBear1 <ethanchavis11@gmail.com>
2025-04-12 16:08:46 -0400

    Fixed bug in the labeling of HTML tags

--- static/script.js
+++ static/script.js
@@ -2,21 +2,15 @@ function getResponse() {
     let userText= $("#textInput").val();
     if (userText != "") {
     let userHtml= '<p class="userText"><span>' + userText + '</span></p>';
-<<<<<<< HEAD
-    $("#textInput").val(""); 
-    $("#chatbox").append(userHtml);
-    document.getElementById('userInput').scrollIntoView({block: 'start', behavior: 'smooth'});
-=======
         $("#textInput").val(""); //clear input
         $("#chatbox").append(userHtml);
         document.getElementById('userInput').scrollIntoView({block: 'start', behavior: 'smooth'});
     }
->>>>>>> a7e5dfcb94db56cea39f264cff8ab588967a5f38
     $.get("/get", { msg: userText }).done(function(data) {
         if (data != "") {
             var botHtml = '<p class="botText"><span>' + data + '</span></p>';
             $("#chatbox").append(botHtml);
-            document.getElementById('#userInput').scrollIntoView({block: 'start', behavior: 'smooth'});
+            document.getElementById('userInput').scrollIntoView({block: 'start', behavior: 'smooth'});
         }
     });
 }
